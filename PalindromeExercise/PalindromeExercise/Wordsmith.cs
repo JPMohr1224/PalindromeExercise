db5f54e950972228d544ab2889e8fa592be9cd28
JP Mohr <James.Mohr2.0@hotmail.com>
2024-05-23 15:15:23 -0400

    palindrome

---
+++ PalindromeExercise/PalindromeExercise/Wordsmith.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class Wordsmith
+	{
+          public bool isAPalindrome(string word)
+            {
+            string reverseDrow = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reverseDrow += word[i];
+            }
+
+            if (word == reverseDrow )
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+    }
+    
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,23 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("tacocat", true)] // ← you will need to put some test data and expected result here.  
+        [InlineData("wow", true)]
+        [InlineData("happy", false)]
 
+        public void isAPalindromeTest(string word, bool expected)
+        {
+            var wordTest = new Wordsmith();
+
+            var actual = wordTest.isAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
